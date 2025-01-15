c2c64640224d0bb78816514987580d746058738f
weyum11234 <wh2484@nyu.edu>
2025-01-15 16:12:52 -0600

    added entry point

--- gone-phishing/index.py
+++ gone-phishing/index.py
@@ -46,3 +46,6 @@ def predict():
         return jsonify(response)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True)
